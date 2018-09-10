e3709e70bbbd608e071bf187f6317e6ed2d9138e
bhperry <bhperry94@gmail.com>
2018-09-10 16:18:37 -0500

    Add TLS components, add template.sh script to generate valid kubernetes YAML

--- README.md
+++ README.md
@@ -2,9 +2,29 @@ Example Deployment
 -----------------
 This is just a simple example deployment for creating a Kubernetes Deployment on a Datica managed K8s cluster. We recommend using this as a baseline to expand off, as it is the most compliant deployment possible (using raw Load Balancers is NOT a good idea). Ingress is only needed if you want to make the service publicly available. Any value in braces is something you should replace, not something that Kubernetes understands.
 
+The ingress host_name should either be a DNS CNAME record pointing to the ingress-controller loadbalancer address, or the loadbalancer address itself for testing. The ingress-controller address can be found under the EXTERNAL-IP section in the output of the following command:
+```sh
+kubectl -n ingress-nginx get svc/ingress-nginx -o wide
+```
+
+The template.sh script can be used to generate valid kubernetes YAML off of the provided templates. The new YAML files will be located at ./{{deployment_name}}/
+
+Use the following command to generate self-signed certificates for development purposes:
+```sh
+openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365
+```
+
 To create the deployment simply run:
 ```sh
+kubectl -n {{deployment_namespace}} create secret tls {{deployment_name}}-tls --cert=path/to/cert.pem --key=path/to/key.pem
 kubectl create -f ./deployment.yaml
 kubeclt create -f ./service.yaml
 kubectl create -f ./ingress.yaml
 ```
+
+If everything is wired up correctly, you should be able to hit your deployment with:
+```sh
+curl -k https://{{host_name}}
+```
+
+NOTE: If you are using self-signed certificates, or certificates signed by a non-public CA, your browser will consider the connection to be insecure, however it will still be encrypted. In order for your browser to recognize your certificates, you will need to use a public certificate authority, such as Let's Encrypt, to create your certificates. For development purposes, it is ok to use self-signed certs and ignore the browser warning.
---
+++ template.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+set -eo pipefail
+
+# Generates kubernetes YAML files off of the templates included in this project
+
+usage() {
+  echo -e "\nUsage: ./template.sh -d mydeployment -n default -i myimage:v2 -p 1234 --hostname my.cname.record
+    Flags:
+      Required:
+        -d, --deployment: Deployment name
+        -n, --namespace: Deployment namespace
+        -i, --image: Deployment image
+        -p, --port: Port to use for the kuberenetes service
+        --hostname: Hostname to use for ingress
+      Optional:
+        -c, --container-port: Port that the container is listening on (default: 80)
+        --port-name: Name of the port (default: http)
+        -h, --help: Print usage"
+
+  exit ${1:-0}
+}
+
+CONTAINER_PORT=80
+PORT_NAME=http
+while [ "$1" ]; do
+  case $1 in
+    -d | --deployment )
+                            shift
+                            DEPLOYMENT_NAME=$1
+                            ;;
+    -n | --namespace )
+                            shift
+                            NAMESPACE=$1
+                            ;;
+    -i | --image )
+                            shift
+                            IMAGE=$1
+                            ;;
+    -p | --port )
+                            shift
+                            PORT=$1
+                            ;;
+    -c | --container-port )
+                            shift
+                            CONTAINER_PORT=$1
+                            ;;
+    --port-name )
+                            shift
+                            PORT_NAME=$1
+                            ;;
+    --hostname )
+                            shift
+                            HOSTNAME=$1
+                            ;;
+    -h | --help )
+                            usage
+                            ;;
+    * )
+                            echo "Error: \"$1\" is not a valid option"
+                            usage 1
+                            ;;
+  esac
+  shift
+done
+
+if [ ! "$DEPLOYMENT_NAME" ] || [ ! "$NAMESPACE" ] || [ ! "$IMAGE" ] || [ ! "$PORT" ] || [ ! "$HOSTNAME" ]; then
+  echo "Error: Missing required arguments"
+  usage 1
+fi
+
+template() {
+  cat $1 | \
+    sed s,{{deployment_name}},"$DEPLOYMENT_NAME",g | \
+    sed s,{{deployment_namespace}},"$NAMESPACE",g | \
+    sed s,{{deployment_image}},"$IMAGE",g | \
+    sed s,{{service_port}},"$PORT",g | \
+    sed s,{{container_port}},"$CONTAINER_PORT",g | \
+    sed s,{{port_name}},"$PORT_NAME",g | \
+    sed s,{{host_name}},"$HOSTNAME",g
+}
+
+mkdir ${DEPLOYMENT_NAME}
+template deployment.yaml > ${DEPLOYMENT_NAME}/deployment.yaml
+template service.yaml > ${DEPLOYMENT_NAME}/service.yaml
+template ingress.yaml > ${DEPLOYMENT_NAME}/ingress.yaml
